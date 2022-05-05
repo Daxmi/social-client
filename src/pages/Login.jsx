2bd642827eec180add6c3a9eb5d02e85f2185641
unknown <ndavidogbonnaya@gmail.com>
2022-05-05 08:59:03 +0100

    changed omitted url

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,15 +4,15 @@ import Github from "../img/github.png";
 
 const Login = () => {
   const google = () => {
-    window.open("http://localhost:5000/auth/google", "_self");
+    window.open("https://social-login-react.herokuapp.com/auth/google", "_self");
   };
 
   const github = () => {
-    window.open("http://localhost:5000/auth/github", "_self");
+    window.open("https://social-login-react.herokuapp.com/auth/github", "_self");
   };
 
   const facebook = () => {
-    window.open("http://localhost:5000/auth/facebook", "_self");
+    window.open("https://social-login-react.herokuapp.com/auth/facebook", "_self");
   };
 
   return (
