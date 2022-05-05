dd4c695160426fe3ae9f81426b6bd7b21d33fd84
unknown <ndavidogbonnaya@gmail.com>
2022-05-05 09:56:52 +0100

    fix cors error

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true,
+         
         },
       })
         .then((response) => {
